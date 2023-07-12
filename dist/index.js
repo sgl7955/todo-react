83eaf40d4a4cc7d1349a6c6d894829bd9f814ced
sgl7955 <sgl7955@gmail.com>
2023-07-12 10:44:03 +0900

    Add files via upload
    TypeScript, Nodejs, TypeORM, MYSQL을 이용해 Backend API 구축

---
+++ dist/index.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+// Instantiate express app
+const app = (0, express_1.default)();
+dotenv_1.default.config();
+// Define sever port
+const port = process.env.PORT;
+// Create a default route.
+app.get("/", (req, res) => {
+    res.send("Express + TypeScript Server");
+});
+// Start listenting to the requests on the defined port
+app.listen(port);
